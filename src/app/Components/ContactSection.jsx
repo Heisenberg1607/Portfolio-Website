5f59cadb125a12d1d309795aebcb32cb60d16900
Heisenberg1607 <atharvakurumbhatte47@gmail.com>
2024-11-13 02:59:19 +0530

    spell check

--- src/app/Components/ContactSection.jsx
+++ src/app/Components/ContactSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import ContactLogos from "../Elements/Contact/ContactLogos";
 import CalendlySection from "../Elements/Contact/CalendlySection";
 import "../globals.css";
@@ -13,16 +13,17 @@ function ContactSection() {
   return (
     <div
       className={`contact-section transition-transform duration-1000 ease-in-out ${
-        isVisible ? 'translate-y-5 opacity-100' : 'translate-y-full opacity-0'
+        isVisible ? "translate-y-5 opacity-100" : "translate-y-full opacity-0"
       }`}
     >
-      <h1 className='text-3xl border-gray-500'>Contact</h1>
-      <p className='mt-3'>
-        Feel free to get in touch and let's have a discussion about how we can work together.
+      <h1 className="text-3xl border-gray-500">Contact</h1>
+      <p className="mt-3">
+        Feel free to get in touch and let&apos;s have a discussion about how we
+        can work together.
       </p>
-      <hr className='mt-5 border-t-1 border-gray-300'></hr>
+      <hr className="mt-5 border-t-1 border-gray-300"></hr>
       <ContactLogos />
-      <hr className='mt-5 border-t-1 border-gray-300'></hr>
+      <hr className="mt-5 border-t-1 border-gray-300"></hr>
       <h1 className="text-2xl my-5">Book a call</h1>
       <CalendlySection />
     </div>
