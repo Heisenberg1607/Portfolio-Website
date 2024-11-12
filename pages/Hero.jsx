b86dad8842b7dd0a6427374b740afedc2f6cf96f
Heisenberg1607 <atharvakurumbhatte47@gmail.com>
2024-11-13 02:53:29 +0530

    spell checks

--- pages/Hero.jsx
+++ pages/Hero.jsx
@@ -1,38 +1,39 @@
-"use client"
+"use client";
 
 // import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 // import Avatar from '@mui/material/Avatar';
-import {  Rocket } from "lucide-react";
+import { Rocket } from "lucide-react";
 import Navigation from "../src/app/Commons/Navigation";
-import '../src/app/Styles/Hero.css'; // Custom CSS for styling
-import {useState, useEffect} from  "react";
+import "../src/app/Styles/Hero.css"; // Custom CSS for styling
+import { useState, useEffect } from "react";
 import "../src/app/globals.css";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Hero() {
-
   const [isVisible, setIsVisible] = useState(false);
-  
 
   useEffect(() => {
     setIsVisible(true);
-  },[]);
+  }, []);
 
   return (
     <div className="hero-container">
       <div className="hero-content">
         {/* Right Section */}
 
-
         <div className="sticky top-0 h-screen p-4">
           <Navigation />
         </div>
         {/* Left Section */}
-        <div className={`contact-section transition-transform duration-1000 ease-in-out p-0 max-w-full text-left ${
-        isVisible ? 'translate-y-5 opacity-100' : 'translate-y-full opacity-0'
-      }`}>
+        <div
+          className={`contact-section transition-transform duration-1000 ease-in-out p-0 max-w-full text-left ${
+            isVisible
+              ? "translate-y-5 opacity-100"
+              : "translate-y-full opacity-0"
+          }`}
+        >
           <Typography variant="h4" gutterBottom className="name-highlight">
             Hey! This is Atharva
           </Typography>
@@ -41,27 +42,35 @@ export default function Hero() {
               <li>Born in Pune, India 🇮🇳</li>
               <li>Currently staying in Fullerton, CA 🇺🇸</li>
             </ul>
-            </div>
+          </div>
           <Typography variant="h6" className="role-title">
             Full Stack Web Developer ● Software Engineer
           </Typography>
           <Typography variant="body1" className="hero-description">
-          Aspiring Software Engineer with a focus on Full Stack development, passionate about building pixel-perfect web experiences. With hands-on experience from two internships, I’m skilled in JavaScript and committed to all aspects of web development. I thrive in collaborative environments, working to create efficient, scalable, and visually engaging web applications.
+            Aspiring Software Engineer with a focus on Full Stack development,
+            passionate about building pixel-perfect web experiences. With
+            hands-on experience from two internships, I’m skilled in JavaScript
+            and committed to all aspects of web development. I thrive in
+            collaborative environments, working to create efficient, scalable,
+            and visually engaging web applications.
           </Typography>
           <hr className="custom-hr" />
           <Typography variant="h6" className="sub-title-home">
-            What I've been working on
+            What I&apos;ve been working on
           </Typography>
           <Typography variant="body1" className="hero-description">
-          I'm Atharva, an international master's student at the California State University, Fullerton, currently pursuing a degree in Computer Science.
+            I&apos;m Atharva, an international master&apos;s student at the
+            California State University, Fullerton, currently pursuing a degree
+            in Computer Science.
           </Typography>
           <div className="work-together-section">
             <Typography variant="h6" className="work-together-title">
-              <Rocket /> Let's Work Together
+              <Rocket /> Let&apos;s Work Together
             </Typography>
             <Typography>
-            I'm seeking a Full-Stack Web Developer / Software Engineer Internship for Summer 2025.
-            Feel free to email me to see how I can contribute.
+              I&apos;m seeking a Full-Stack Web Developer / Software Engineer
+              Internship for Summer 2025. Feel free to email me to see how I can
+              contribute.
             </Typography>
             <Link href="/Contacts" passHref>
               <Button variant="contained" className="work-together-btn">
