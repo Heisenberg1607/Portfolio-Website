b5dfd1a458f9530788e783588ca9ad67eb23c67b
Heisenberg1607 <atharvakurumbhatte47@gmail.com>
2024-11-13 05:54:50 +0530

    images

--- src/app/Commons/EducationCard.jsx
+++ src/app/Commons/EducationCard.jsx
@@ -1,13 +1,18 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 function EducationCard(props) {
   return (
     <div className="flex items-center bg-gray-900 text-white p-5 rounded-lg mb-5 shadow-lg my-6 max-500:flex-col  max-500:p-3">
       <div className="w-16 h-16  rounded-full flex items-center justify-center mr-5 max-500:flex max-500:items-center ">
-      <Image alt="logo" src={`/icons/${props.icon}`} width={65} height={65}></Image>
+        <Image
+          alt="logo"
+          src={`/Icons/${props.icon}`}
+          width={65}
+          height={65}
+        ></Image>
       </div>
       <div className="edu-info max-500:text-left max-500:w-full ">
         <h3 className="text-xl text-gray-1000">{props.name}</h3>
--- src/app/Elements/Contact/ContactLogos.jsx
+++ src/app/Elements/Contact/ContactLogos.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+// import Google from "/Icons/google-gmail.svg";
 
 function ContactLogos() {
   return (
     <div className=" my-6">
-      <h2 className="text-white text-2xl font-semibold mb-4">Find me on social media</h2>
+      <h2 className="text-white text-2xl font-semibold mb-4">
+        Find me on social media
+      </h2>
       <div className="flex justify-center space-x-4 max-500:flex-col max-500:items-center gap-3">
-        
         {/* Email Button */}
         <a
           href="mailto:athru@csu.fullerton.edu"
@@ -13,18 +16,28 @@ function ContactLogos() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="/Icons/google-gmail.svg" alt="Email Icon" className="w-5 h-5 mr-2 filter invert" />
+          <Image
+            src="/Icons/google-gmail.svg"
+            alt="Email Icon"
+            className="w-5 h-5 mr-2 filter invert"
+            width="200"
+            height="200"
+          />
           Email
         </a>
 
         {/* LinkedIn Button */}
-        <a 
+        <a
           href="https://www.linkedin.com/in/atharva-kurumbhatte/"
           className="w-full flex items-center justify-center w-32 h-12 bg-blue-700 text-white font-semibold rounded-md hover:bg-blue-800 border border-white"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="/Icons/linkedin-icon.svg" alt="LinkedIn Icon" className="w-5 h-5 mr-2" />
+          <img
+            src="/Icons/linkedin-icon.svg"
+            alt="LinkedIn Icon"
+            className="w-5 h-5 mr-2"
+          />
           LinkedIn
         </a>
 
@@ -41,15 +54,18 @@ function ContactLogos() {
 
         {/* Instagram Button */}
         <a
-  href="https://www.instagram.com/xtharva01/"
-  className="w-full flex items-center text-2xs justify-center w-32 h-12 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 text-white font-semibold rounded-md hover:opacity-90 border border-white"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <img src="/Icons/instagram-icon.svg" alt="Instagram Icon" className="w-5 h-5 mr-2" />
-  Instagram
-</a>
-
+          href="https://www.instagram.com/xtharva01/"
+          className="w-full flex items-center text-2xs justify-center w-32 h-12 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 text-white font-semibold rounded-md hover:opacity-90 border border-white"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Icons/instagram-icon.svg"
+            alt="Instagram Icon"
+            className="w-5 h-5 mr-2"
+          />
+          Instagram
+        </a>
 
         {/* GitHub Button */}
         <a
@@ -58,13 +74,16 @@ function ContactLogos() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="/Icons/github-icon.svg" alt="GitHub Icon" className="w-5 h-5 mr-2" />
+          <img
+            src="/Icons/github-icon.svg"
+            alt="GitHub Icon"
+            className="w-5 h-5 mr-2"
+          />
           GitHub
         </a>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default ContactLogos
+export default ContactLogos;
