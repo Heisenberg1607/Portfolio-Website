a555793c6b17567aaf169b5d2116073852e13e60
Heisenberg1607 <atharvakurumbhatte47@gmail.com>
2025-01-10 11:37:56 +0530

    google analytics issue solved

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,24 +1,33 @@
-"use client"
+"use client";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-
   apiKey: process.env.API_KEY,
   authDomain: "portfolio-nextjs-54a7b.firebaseapp.com",
   projectId: "portfolio-nextjs-54a7b",
   storageBucket: "portfolio-nextjs-54a7b.firebasestorage.app",
   messagingSenderId: "61168810621",
   appId: "1:61168810621:web:33901cb2a89a1cb9fc4f14",
-  measurementId: "G-C4NM8C8NFM"
+  measurementId: "G-C4NM8C8NFM",
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
-console.log(analytics);
 
-export { db }
+// Initialize Analytics only in the browser and if supported
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+      console.log("Analytics initialized:", analytics);
+    } else {
+      console.log("Analytics is not supported in this environment.");
+    }
+  });
+}
+
+export { db, analytics };
